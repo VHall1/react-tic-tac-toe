332c7ad45e9c0df78d7fc626d1629d4ca86cad57
VHall1 <victorhallpsn@hotmail.com>
2023-08-02 17:43:15 +0100

    tweak cy tests

--- cypress/e2e/tic-tac-toe.cy.js
+++ cypress/e2e/tic-tac-toe.cy.js
@@ -1,17 +1,23 @@
-describe("anonymous tic-tac-toe", () => {
+describe("tic-tac-toe", () => {
   it("works with ties", () => {
     cy.visit("/");
-
-    cy.findByTestId("square-0").click();
-    cy.findByTestId("square-2").click();
-    cy.findByTestId("square-1").click();
-    cy.findByTestId("square-3").click();
-    cy.findByTestId("square-4").click();
-    cy.findByTestId("square-7").click();
-    cy.findByTestId("square-5").click();
-    cy.findByTestId("square-8").click();
-    cy.findByTestId("square-6").click();
-
+    cyClickSquares([0, 2, 1, 3, 4, 7, 5, 8, 6]);
     cy.get(".status").should("have.text", "Tie");
   });
+
+  it("works when X wins", () => {
+    cy.visit("/");
+    cyClickSquares([0, 1, 6, 3, 8, 7, 4]);
+    cy.get(".status").should("have.text", "Winner: X");
+  });
+
+  it("works when O wins", () => {
+    cy.visit("/");
+    cyClickSquares([1, 0, 2, 3, 5, 6]);
+    cy.get(".status").should("have.text", "Winner: O");
+  });
 });
+
+function cyClickSquares(squares) {
+  squares.forEach((pos) => cy.findByTestId(`square-${pos}`).click());
+}
