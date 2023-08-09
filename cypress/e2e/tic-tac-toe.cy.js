7ec0e554d1e55da48e009539d47c7f848300429a
VHall1 <victorhallpsn@hotmail.com>
2023-08-09 20:03:44 +0100

    update tests

--- cypress/e2e/tic-tac-toe.cy.js
+++ cypress/e2e/tic-tac-toe.cy.js
@@ -2,19 +2,19 @@ describe("tic-tac-toe", () => {
   it("works with ties", () => {
     cy.visit("/");
     cyClickSquares([0, 2, 1, 3, 4, 7, 5, 8, 6]);
-    cy.get(".status").should("have.text", "Tie");
+    cy.findByTestId("status").should("have.text", "Tie");
   });
 
   it("works when X wins", () => {
     cy.visit("/");
     cyClickSquares([0, 1, 6, 3, 8, 7, 4]);
-    cy.get(".status").should("have.text", "Winner: X");
+    cy.findByTestId("status").should("have.text", "Winner: X");
   });
 
   it("works when O wins", () => {
     cy.visit("/");
     cyClickSquares([1, 0, 2, 3, 5, 6]);
-    cy.get(".status").should("have.text", "Winner: O");
+    cy.findByTestId("status").should("have.text", "Winner: O");
   });
 });
 
